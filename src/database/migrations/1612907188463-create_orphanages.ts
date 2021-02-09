a58a78e229d568bfcd943fa81d1a6cce48a4b557
Brenno Santos <bsantos@primeit.pt>
2021-02-09 22:46:42 +0000

    create orphanages model and controller

--- src/database/migrations/1612907188463-create_orphanages.ts
+++ src/database/migrations/1612907188463-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1612907188463 implements MigrationInterface {
           type: 'text',
         },
         {
+          name: 'opening_hours',
+          type: 'varchar',
+        },
+        {
           name: 'open_on_weekends',
           type: 'boolean',
           default: false,
