d5631fb2b00bddd81fd92a2ae15b44d04eb75a4b
Brenno Santos <bsantos@primeit.pt>
2021-02-09 23:32:28 +0000

    configure views

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -1,15 +1,18 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 
+import orphanageView from '../views/orphanages_view';
 import Orphanage from '../models/Orphanage';
 
 export default {
   async index(req: Request, res: Response) {
     const orphanageRepository = getRepository(Orphanage);
 
-    const orphanages = await orphanageRepository.find();
+    const orphanages = await orphanageRepository.find({
+      relations: ['images']
+    });
 
-    return res.json(orphanages);
+    return res.json(orphanageView.renderMany(orphanages));
   },
 
   async show(req: Request, res: Response) {
@@ -17,9 +20,11 @@ export default {
 
     const orphanageRepository = getRepository(Orphanage);
 
-    const orphanage = await orphanageRepository.findOneOrFail(id);
+    const orphanage = await orphanageRepository.findOneOrFail(id, {
+      relations: ['images']
+    });
 
-    return res.json(orphanage);
+    return res.json(orphanageView.render(orphanage));
   },
 
   async create(req: Request, res: Response) {
